e6fe446fd0cd5ae5f1f83789ead20e01e4168852
Stenstromen <kontakt@filipstenstrom.se>
2026-04-13 21:14:49 +0200

    migrate to opennextjs

--- src/app/api/quotes/random/route.ts
+++ src/app/api/quotes/random/route.ts
@@ -1,7 +1,6 @@
 import { quotes } from '../data';
 import { NextResponse } from 'next/server';
 
-export const runtime = 'edge';
 // GET /api/quotes/random - Returns a random quote
 export async function GET() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
--- src/app/api/quotes/route.ts
+++ src/app/api/quotes/route.ts
@@ -1,7 +1,6 @@
 import { quotes } from './data';
 import { NextResponse } from 'next/server';
 
-export const runtime = 'edge';
 // GET /api/quotes - Returns all quotes
 export async function GET() {
   return NextResponse.json(quotes);
